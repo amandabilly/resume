194283c6372afd1b695788af9036eb1fe2fbcc7a
Amanda Billy <amandacbilly@gmail.com>
2014-04-15 17:07:14 -0400

    Updating the README and the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,69 +3,68 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
+    sass: {
       dist: {
-        src: ['js/*.js'],
-        dest: 'dist/js/main.js'
-      }
-    },
-    jshint: {
-      beforeconcat: ['js/*.js'],
-      afterconcat: ['dist/js/*.js']
-    },
-    sass: {                              // Task
-      dist: {                            // Target
-        options: {                       // Target options
+        options: {
           style: 'expanded'
         },
         files: [{
           expand: true,
           cwd: 'sass',
           src: ['*.scss'],
-          dest: 'dist/css',
+          dest: 'sass/css',
           ext: '.css'
         }]
       }
     },
-    csslint: {
-      strict: {
-        options: {
-          import: 2
-        },
-        src: ['sass/*.css']
+    concat: {
+      js: {
+        src: ['js/bootstrap.js', 'js/jquery.simple-text-rotator.js', 'js/jquery.sticky-kit.js', 'js/enquire.min.js', 'js/custom.js'],
+        dest: 'dist/js/main.js'
       },
-      lax: {
-        options: {
-          import: false
-        },
-        src: ['sass/*.css']
+      css: {
+        src: ['sass/css/bootstrap.css', 'sass/css/simpletextrotator.css', 'sass/css/custom.css'],
+        dest: 'dist/css/main.css'
       }
+    },
+    jshint: {
+      all: ['js/custom.js']
     },
     autoprefixer: {
       multiple_files: {
         expand: true,
         flatten: true,
-        src: 'sass/*.css', // -> src/css/file1.css, src/css/file2.css
-        dest: 'dist/css/' // -> dest/css/file1.css, dest/css/file2.css
+        src: 'dist/css/*.css', // -> src/css/file1.css, src/css/file2.css
+        dest: 'dist/css' // -> dest/css/file1.css, dest/css/file2.css
       },
+    },
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '/* My minified css file */'
+        },
+        files: {
+          'dist/css/main.min.css': ['dist/css/main.css']
+        }
+      }
     },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      build: {
-        src: 'js/<%= pkg.name %>.js',
-        dest: 'dist/js/<%= pkg.name %>.min.js'
+      js: {
+        src: 'dist/js/main.js',
+        dest: 'dist/js/main.min.js'
       }
     },
     watch: {
       css: {
         files: ['sass/*.scss'],
-        tasks: ['sass', 'csslint', 'autoprefixer']
+        tasks: ['sass', 'concat', 'autoprefixer', 'cssmin']
       },
       scripts: {
         files: ['js/*.js'],
-        tasks: ['jshint']
+        tasks: ['jshint', 'concat']
       },
       disembiggen: {
         files: ['dist/*/*.js', 'dist/*/*.css'],
@@ -80,16 +79,16 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   // Load the plugin that provides the "sass" task.
   grunt.loadNpmTasks('grunt-contrib-sass');
-  // Load the plugin that provides the "csslint" task.
-  grunt.loadNpmTasks('grunt-contrib-csslint');
   // Load the plugin that provides the "autoprefixer" task.
   grunt.loadNpmTasks('grunt-autoprefixer');
+  // Load the plugin that provides the "cssmin" task.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   // Load the plugin that provides the "watch" task.
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'jshint', 'sass', 'csslint', 'autoprefixer', 'uglify', 'watch']);
+  grunt.registerTask('default', ['sass', 'concat', 'jshint', 'autoprefixer', 'cssmin', 'uglify', 'watch']);
 
 };
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Building my responsive resume with Grunt.js, Compass, and full version control.
+Resume
+======
+
+This is my personal resume. I may release this as a template eventually. Who knows?
+
+Included
+--------
+
+* dist = The files in use on the live site, including the HTML, images, fonts, and minified style and script files.
+* doc = README files and license information for the plugins and fonts used throughout the project.
+* js = The original JS files and plugins.
+* sass = The .scss files and the converted CSS files that have not been concatenated yet.
+* Gruntfile.js = My Grunt plugins and settings.
