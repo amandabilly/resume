353bfaa8097185404a2bbe58c0864c86702a9479
Amanda Billy <amandacbilly@gmail.com>
2014-04-15 17:00:33 -0400

    Adding production files

---
+++ dist/js/main.js
@@ -0,0 +1,392 @@
+//= require bootstrap/affix
+//= require bootstrap/alert
+//= require bootstrap/button
+//= require bootstrap/carousel
+//= require bootstrap/collapse
+//= require bootstrap/dropdown
+//= require bootstrap/tab
+//= require bootstrap/transition
+//= require bootstrap/scrollspy
+//= require bootstrap/modal
+//= require bootstrap/tooltip
+//= require bootstrap/popover
+
+/* ===========================================================
+ * jquery-simple-text-rotator.js v1
+ * ===========================================================
+ * Copyright 2013 Pete Rojwongsuriya.
+ * http://www.thepetedesign.com
+ *
+ * A very simple and light weight jQuery plugin that 
+ * allows you to rotate multiple text without changing 
+ * the layout
+ * https://github.com/peachananr/simple-text-rotator
+ *
+ * ========================================================== */
+
+!function($){
+  
+  var defaults = {
+		animation: "dissolve",
+		separator: ",",
+		speed: 2000
+	};
+	
+	$.fx.step.textShadowBlur = function(fx) {
+    $(fx.elem).prop('textShadowBlur', fx.now).css({textShadow: '0 0 ' + Math.floor(fx.now) + 'px black'});
+  };
+	
+  $.fn.textrotator = function(options){
+    var settings = $.extend({}, defaults, options);
+    
+    return this.each(function(){
+      var el = $(this)
+      var array = [];
+      $.each(el.text().split(settings.separator), function(key, value) { 
+        array.push(value); 
+      });
+      el.text(array[0]);
+      
+      // animation option
+      var rotate = function() {
+        switch (settings.animation) {
+          case 'dissolve':
+            el.animate({
+              textShadowBlur:20,
+              opacity: 0
+            }, 500 , function() {
+              index = $.inArray(el.text(), array)
+              if((index + 1) == array.length) index = -1
+              el.text(array[index + 1]).animate({
+                textShadowBlur:0,
+                opacity: 1
+              }, 500 );
+            });
+          break;
+          
+          case 'flip':
+            if(el.find(".back").length > 0) {
+              el.html(el.find(".back").html())
+            }
+          
+            var initial = el.text()
+            var index = $.inArray(initial, array)
+            if((index + 1) == array.length) index = -1
+            
+            el.html("");
+            $("<span class='front'>" + initial + "</span>").appendTo(el);
+            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(el);
+            el.wrapInner("<span class='rotating' />").find(".rotating").hide().addClass("flip").show().css({
+              "-webkit-transform": " rotateY(-180deg)",
+              "-moz-transform": " rotateY(-180deg)",
+              "-o-transform": " rotateY(-180deg)",
+              "transform": " rotateY(-180deg)"
+            })
+          break;
+          
+          case 'flipUp':
+            if(el.find(".back").length > 0) {
+              el.html(el.find(".back").html())
+            }
+          
+            var initial = el.text()
+            var index = $.inArray(initial, array)
+            if((index + 1) == array.length) index = -1
+            
+            el.html("");
+            $("<span class='front'>" + initial + "</span>").appendTo(el);
+            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(el);
+            el.wrapInner("<span class='rotating' />").find(".rotating").hide().addClass("flip up").show().css({
+              "-webkit-transform": " rotateX(-180deg)",
+              "-moz-transform": " rotateX(-180deg)",
+              "-o-transform": " rotateX(-180deg)",
+              "transform": " rotateX(-180deg)"
+            })
+          break;
+          
+          case 'flipCube':
+            if(el.find(".back").length > 0) {
+              el.html(el.find(".back").html())
+            }
+          
+            var initial = el.text()
+            var index = $.inArray(initial, array)
+            if((index + 1) == array.length) index = -1
+            
+            el.html("");
+            $("<span class='front'>" + initial + "</span>").appendTo(el);
+            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(el);
+            el.wrapInner("<span class='rotating' />").find(".rotating").hide().addClass("flip cube").show().css({
+              "-webkit-transform": " rotateY(180deg)",
+              "-moz-transform": " rotateY(180deg)",
+              "-o-transform": " rotateY(180deg)",
+              "transform": " rotateY(180deg)"
+            })
+          break;
+          
+          case 'flipCubeUp':
+            if(el.find(".back").length > 0) {
+              el.html(el.find(".back").html())
+            }
+          
+            var initial = el.text()
+            var index = $.inArray(initial, array)
+            if((index + 1) == array.length) index = -1
+            
+            el.html("");
+            $("<span class='front'>" + initial + "</span>").appendTo(el);
+            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(el);
+            el.wrapInner("<span class='rotating' />").find(".rotating").hide().addClass("flip cube up").show().css({
+              "-webkit-transform": " rotateX(180deg)",
+              "-moz-transform": " rotateX(180deg)",
+              "-o-transform": " rotateX(180deg)",
+              "transform": " rotateX(180deg)"
+            })
+          break;
+          
+          case 'spin':
+            if(el.find(".rotating").length > 0) {
+              el.html(el.find(".rotating").html())
+            }
+            index = $.inArray(el.text(), array)
+            if((index + 1) == array.length) index = -1
+            
+            el.wrapInner("<span class='rotating spin' />").find(".rotating").hide().text(array[index + 1]).show().css({
+              "-webkit-transform": " rotate(0) scale(1)",
+              "-moz-transform": "rotate(0) scale(1)",
+              "-o-transform": "rotate(0) scale(1)",
+              "transform": "rotate(0) scale(1)"
+            })
+          break;
+          
+          case 'fade':
+            el.fadeOut(settings.speed, function() {
+              index = $.inArray(el.text(), array)
+              if((index + 1) == array.length) index = -1
+              el.text(array[index + 1]).fadeIn(settings.speed);
+            });
+          break;
+        }
+      };
+      setInterval(rotate, settings.speed);
+    });
+  }
+  
+}(window.jQuery);
+
+
+
+// Generated by CoffeeScript 1.7.1
+
+/**
+@license Sticky-kit v1.0.4 | WTFPL | Leaf Corcoran 2014 | http://leafo.net
+ */
+
+(function() {
+  var $, win;
+
+  $ = this.jQuery;
+
+  win = $(window);
+
+  $.fn.stick_in_parent = function(opts) {
+    var elm, inner_scrolling, offset_top, parent_selector, sticky_class, _fn, _i, _len;
+    if (opts == null) {
+      opts = {};
+    }
+    sticky_class = opts.sticky_class, inner_scrolling = opts.inner_scrolling, parent_selector = opts.parent, offset_top = opts.offset_top;
+    if (offset_top == null) {
+      offset_top = 0;
+    }
+    if (parent_selector == null) {
+      parent_selector = void 0;
+    }
+    if (inner_scrolling == null) {
+      inner_scrolling = true;
+    }
+    if (sticky_class == null) {
+      sticky_class = "is_stuck";
+    }
+    _fn = function(elm, padding_bottom, parent_top, parent_height, top, height, el_float) {
+      var bottomed, detach, fixed, last_pos, offset, parent, recalc, recalc_and_tick, spacer, tick;
+      if (elm.data("sticky_kit")) {
+        return;
+      }
+      elm.data("sticky_kit", true);
+      parent = elm.parent();
+      if (parent_selector != null) {
+        parent = parent.closest(parent_selector);
+      }
+      if (!parent.length) {
+        throw "failed to find stick parent";
+      }
+      fixed = false;
+      bottomed = false;
+      spacer = $("<div />");
+      spacer.css('position', elm.css('position'));
+      recalc = function() {
+        var border_top, padding_top, restore;
+        border_top = parseInt(parent.css("border-top-width"), 10);
+        padding_top = parseInt(parent.css("padding-top"), 10);
+        padding_bottom = parseInt(parent.css("padding-bottom"), 10);
+        parent_top = parent.offset().top + border_top + padding_top;
+        parent_height = parent.height();
+        restore = fixed ? (fixed = false, bottomed = false, elm.insertAfter(spacer).css({
+          position: "",
+          top: "",
+          width: "",
+          bottom: ""
+        }), spacer.detach(), true) : void 0;
+        top = elm.offset().top - parseInt(elm.css("margin-top"), 10) - offset_top;
+        height = elm.outerHeight(true);
+        el_float = elm.css("float");
+        spacer.css({
+          width: elm.outerWidth(true),
+          height: height,
+          display: elm.css("display"),
+          "vertical-align": elm.css("vertical-align"),
+          "float": el_float
+        });
+        if (restore) {
+          return tick();
+        }
+      };
+      recalc();
+      if (height === parent_height) {
+        return;
+      }
+      last_pos = void 0;
+      offset = offset_top;
+      tick = function() {
+        var css, delta, scroll, will_bottom, win_height;
+        scroll = win.scrollTop();
+        if (last_pos != null) {
+          delta = scroll - last_pos;
+        }
+        last_pos = scroll;
+        if (fixed) {
+          will_bottom = scroll + height + offset > parent_height + parent_top;
+          if (bottomed && !will_bottom) {
+            bottomed = false;
+            elm.css({
+              position: "fixed",
+              bottom: "",
+              top: offset
+            }).trigger("sticky_kit:unbottom");
+          }
+          if (scroll < top) {
+            fixed = false;
+            offset = offset_top;
+            if (el_float === "left" || el_float === "right") {
+              elm.insertAfter(spacer);
+            }
+            spacer.detach();
+            css = {
+              position: "",
+              width: "",
+              top: ""
+            };
+            elm.css(css).removeClass(sticky_class).trigger("sticky_kit:unstick");
+          }
+          if (inner_scrolling) {
+            win_height = win.height();
+            if (height > win_height) {
+              if (!bottomed) {
+                offset -= delta;
+                offset = Math.max(win_height - height, offset);
+                offset = Math.min(offset_top, offset);
+                if (fixed) {
+                  elm.css({
+                    top: offset + "px"
+                  });
+                }
+              }
+            }
+          }
+        } else {
+          if (scroll > top) {
+            fixed = true;
+            css = {
+              position: "fixed",
+              top: offset
+            };
+            css.width = elm.css("box-sizing") === "border-box" ? elm.outerWidth() + "px" : elm.width() + "px";
+            elm.css(css).addClass(sticky_class).after(spacer);
+            if (el_float === "left" || el_float === "right") {
+              spacer.append(elm);
+            }
+            elm.trigger("sticky_kit:stick");
+          }
+        }
+        if (fixed) {
+          if (will_bottom == null) {
+            will_bottom = scroll + height + offset > parent_height + parent_top;
+          }
+          if (!bottomed && will_bottom) {
+            bottomed = true;
+            if (parent.css("position") === "static") {
+              parent.css({
+                position: "relative"
+              });
+            }
+            return elm.css({
+              position: "absolute",
+              bottom: padding_bottom,
+              top: "auto"
+            }).trigger("sticky_kit:bottom");
+          }
+        }
+      };
+      recalc_and_tick = function() {
+        recalc();
+        return tick();
+      };
+      detach = function() {
+        win.off("scroll", tick);
+        $(document.body).off("sticky_kit:recalc", recalc_and_tick);
+        elm.off("sticky_kit:detach", detach);
+        elm.removeData("sticky_kit");
+        elm.css({
+          position: "",
+          bottom: "",
+          top: ""
+        });
+        parent.position("position", "");
+        if (fixed) {
+          elm.insertAfter(spacer).removeClass(sticky_class);
+          return spacer.remove();
+        }
+      };
+      win.on("touchmove", tick);
+      win.on("scroll", tick);
+      win.on("resize", recalc_and_tick);
+      $(document.body).on("sticky_kit:recalc", recalc_and_tick);
+      elm.on("sticky_kit:detach", detach);
+      return setTimeout(tick, 0);
+    };
+    for (_i = 0, _len = this.length; _i < _len; _i++) {
+      elm = this[_i];
+      _fn($(elm));
+    }
+    return this;
+  };
+
+}).call(this);
+
+/*!
+ * enquire.js v2.1.0 - Awesome Media Queries in JavaScript
+ * Copyright (c) 2014 Nick Williams - http://wicky.nillia.ms/enquire.js
+ * License: MIT (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+!function(a,b,c){var d=window.matchMedia;"undefined"!=typeof module&&module.exports?module.exports=c(d):"function"==typeof define&&define.amd?define(function(){return b[a]=c(d)}):b[a]=c(d)}("enquire",this,function(a){"use strict";function b(a,b){var c,d=0,e=a.length;for(d;e>d&&(c=b(a[d],d),c!==!1);d++);}function c(a){return"[object Array]"===Object.prototype.toString.apply(a)}function d(a){return"function"==typeof a}function e(a){this.options=a,!a.deferSetup&&this.setup()}function f(b,c){this.query=b,this.isUnconditional=c,this.handlers=[],this.mql=a(b);var d=this;this.listener=function(a){d.mql=a,d.assess()},this.mql.addListener(this.listener)}function g(){if(!a)throw new Error("matchMedia not present, legacy browsers require a polyfill");this.queries={},this.browserIsIncapable=!a("only all").matches}return e.prototype={setup:function(){this.options.setup&&this.options.setup(),this.initialised=!0},on:function(){!this.initialised&&this.setup(),this.options.match&&this.options.match()},off:function(){this.options.unmatch&&this.options.unmatch()},destroy:function(){this.options.destroy?this.options.destroy():this.off()},equals:function(a){return this.options===a||this.options.match===a}},f.prototype={addHandler:function(a){var b=new e(a);this.handlers.push(b),this.matches()&&b.on()},removeHandler:function(a){var c=this.handlers;b(c,function(b,d){return b.equals(a)?(b.destroy(),!c.splice(d,1)):void 0})},matches:function(){return this.mql.matches||this.isUnconditional},clear:function(){b(this.handlers,function(a){a.destroy()}),this.mql.removeListener(this.listener),this.handlers.length=0},assess:function(){var a=this.matches()?"on":"off";b(this.handlers,function(b){b[a]()})}},g.prototype={register:function(a,e,g){var h=this.queries,i=g&&this.browserIsIncapable;return h[a]||(h[a]=new f(a,i)),d(e)&&(e={match:e}),c(e)||(e=[e]),b(e,function(b){h[a].addHandler(b)}),this},unregister:function(a,b){var c=this.queries[a];return c&&(b?c.removeHandler(b):(c.clear(),delete this.queries[a])),this}},new g});
+$(document).ready(function() {
+  $(".rotate").textrotator({
+    animation: "fade", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+    separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+    speed: 4000 // How many milliseconds until the next word show.
+  });
+  enquire.register("screen and (min-width:47em)", function() {
+    $("#side").stick_in_parent();
+  });
+});
